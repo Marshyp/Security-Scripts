957bdd240ea2c7424f9922bf195b87a0c1df0b37
Philip Marsh <130765345+Marshyp@users.noreply.github.com>
2025-01-02 14:00:00 +0000

    Create Get-LocalAdmins.ps1

---
+++ Servers/Get-LocalAdmins.ps1
@@ -0,0 +1,12 @@
+﻿invoke-command { 
+$members = net localgroup administrators | 
+    where {$_ -AND $_ -notmatch "command completed successfully"} | 
+    select -skip 4
+New-Object PSObject -Property @{ 
+    Computername = $env:COMPUTERNAME 
+    Group = "Administrators"
+    Members=$members
+    status = $status
+    } 
+   } -computer (Get-Content c:\_scripts\LocalAdmins\servers.txt) | 
+   Select * -ExcludeProperty RunspaceID | Export-CSV c:\_scripts\LocalAdmins\local_admins.csv -NoTypeInformation
